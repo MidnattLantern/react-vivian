a6848776a9b5346381b8129b394b80f4081e1f01
Alma <isakvent@outlook.com>
2024-07-07 19:35:38 +0200

    switch axios base url

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 /* one baseURL is for development, the other for Heroku deployment */
-axios.defaults.baseURL = 'http://127.0.0.1:8000/';
-//axios.defaults.baseURL = 'https://vivian-api-740f908fe2d0.herokuapp.com/';
+//axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.baseURL = 'https://vivian-api-740f908fe2d0.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
